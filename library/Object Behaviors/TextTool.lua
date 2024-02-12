620fec4dd5b535d1867e2761b85575d5ae1567b5
Benjamin Valpey <bavalpey@users.noreply.github.com>
2024-02-11 23:17:29 -0600

    Add definition file for the TextTool class

--- library/Object Behaviors/TextTool.lua
+++ library/Object Behaviors/TextTool.lua
@@ -1,3 +1,43 @@
 ---@meta TextTool
 
+---The `TextTool` behavior is present on the text objects created with the [text tool](https://kb.tabletopsimulator.com/game-tools/text-tool/).
+---
+---[Open Documentation](https://api.tabletopsimulator.com/behavior/texttool/)
 ---@class TextTool
+local TextTool = {}
+
+---
+---Return the Color of the font
+---@return Color
+function TextTool.getColor() end
+
+---
+---Return the font size
+---@return int
+function TextTool.getFontSize() end
+
+---
+---Return the current text. Behaves the same as Object's [getValue()](https://api.tabletopsimulator.com/object/#getvalue).
+---@return string
+---
+---***
+---@see Object.getValue
+function TextTool.getValue() end
+
+---
+---Set the color of the font.
+---@param color Color
+---@return bool
+function TextTool.setColor(color) end
+
+---
+---Set the font size.
+---@param size int
+---@return bool
+function TextTool.setFontSize(size) end
+
+---
+---Set the current text. Behaves the same as Object's [setValue()](https://api.tabletopsimulator.com/object/#setvalue).
+---@param value string
+---@return bool
+function TextTool.setValue(value) end

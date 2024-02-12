63308d6f5e0b0c974ee083455ca3a2a57e776e61
Benjamin Valpey <bavalpey@users.noreply.github.com>
2024-02-12 12:07:15 -0600

    Refactor definition file for the AssetBundle class

--- library/Object Behaviors/AssetBundle.lua
+++ library/Object Behaviors/AssetBundle.lua
@@ -1,39 +1,77 @@
 ---@meta AssetBundle
 
 ---The AssetBundle behavior is present on Objects that were created from a custom [AssetBundle](https://kb.tabletopsimulator.com/custom-content/custom-assetbundle/).
+---
+---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/)
 ---@class AssetBundle
----@field getLoopingEffectIndex fun(): int Index of the currently looping effect. Indexes starts at 0.
+local AssetBundle = {}
+
+---Return the index of the currently looping effect. Indexes starts at 0.
+---@return int
+---
+---***
+---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/#getloopingeffectindex)
+---
+function AssetBundle.getLoopingEffectIndex() end
+
 ---Returns a Table with the keys "index" and "name" for each looping effect.
+---@return {index: int, name: int}[]
+---
+---***
+---
+---### Example Usage
 ---```
----    -- Example usage
----    effectTable = self.AssetBundle.getLoopingEffects()
+---effectTable = self.AssetBundle.getLoopingEffects()
+---
+----- Example returned table
+---{
+---    {index=0, name="Effect Name 1"},
+---    {index=1, name="Effect Name 2"},
+---}
 ---```
----```
----    -- Example returned table
----    {
----        {index=0, name="Effect Name 1"},
----        {index=1, name="Effect Name 2"},
----    }
----```
+---
 ---***
 ---
 ---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/#getloopingeffects)
----@field getLoopingEffets {index: int, name: int}[]
+---
+function AssetBundle.getLoopingEffects() end
+
+
 ---Returns a Table with the keys "index" and "name" for each trigger effect.
+---@return {index: int, name: int}[]
+---
+---***
+---
+---### Example Usage
 ---```
----    -- Example usage
----    effectTable = self.AssetBundle.getTriggerEffects()
----```
----```
----    -- Example returned table
----    {
----        {index=0, name="Effect Name 1"},
----        {index=1, name="Effect Name 2"},
----    }
+---effectTable = self.AssetBundle.getTriggerEffects()
+----- Example returned table
+---{
+---    {index=0, name="Effect Name 1"},
+---    {index=1, name="Effect Name 2"},
+---}
 ---```
 ---
 ---***
+---
 ---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/#getTriggerEffects)
----@field getTriggerEffects {index: int, name: int}[] Returns a Table with the keys "index" and "name" for each trigger effect.
----@field playLoopingEffect fun(index: int): nil Starts playing a looping effect. Indexes starts at 0.
----@field playTriggerEffect fun(index: int): nil Starts playing a trigger effect. Indexes starts at 0.
+---
+function AssetBundle.getTriggerEffects() end
+
+---Starts playing a looping effect. Indexes start at 0.
+---@param index int
+---@return nil
+---
+---***
+---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/#playloopingeffect]
+---
+function AssetBundle.playLoopingEffect(index) end
+
+---Starts playing a trigger effect. Indexes start at 0.
+---@param index int
+---@return nil
+---
+---***
+---[Open Documentation](https://api.tabletopsimulator.com/behavior/assetbundle/#playtriggereffect]
+---
+function AssetBundle.playTriggerEffect(index) end

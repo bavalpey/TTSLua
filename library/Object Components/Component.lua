535dbca61636d2d7f028b6ed60499374a78c8729
Benjamin Valpey <bavalpey@users.noreply.github.com>
2024-02-12 12:12:03 -0600

    Refactor definition file for the Component class

--- library/Object Components/Component.lua
+++ library/Object Components/Component.lua
@@ -6,6 +6,31 @@
 ---@class Component
 ---@field game_object GameObject The GameObject the Component composes.	
 ---@field name string The name of the Component.
----@field get fun(name: string): any Obtains the value of a given Variable on a Component.
----@field getVars fun():table<string, string> 	Returns a table mapping Var names (`string`) to their type, which is also represented as a (`string`).
----@field set fun(name: string, value: any): bool Sets the Var of the specified `name` to the provided `value`.
+local Component = {}
+
+---Obtain the value of a given Variable on a Component.
+---@param name string
+---@return any
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/component/#get)
+function Component.get(name) end
+
+---Returns a table mapping Var names to their type.
+---@return table<string, string>
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/component/#getvars)
+function Component.getVars() end
+
+---Sets the Var of the specified `name` to the provided `value`.
+---@param name string
+---@param value any
+---@return bool
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/component/#set)
+function Component.set(name, value) end

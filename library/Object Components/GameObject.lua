e9de7e6ba3294e6cfb1a4ac662501b4035c170a5
Benjamin Valpey <bavalpey@users.noreply.github.com>
2024-02-12 12:19:57 -0600

    Refactor definition files for the GameObject class

--- library/Object Components/GameObject.lua
+++ library/Object Components/GameObject.lua
@@ -1,40 +1,116 @@
 ---@meta Object_Components
 
 ---# Introduction
---->### Danger
+--->### 🚨 Danger
 --->Component APIs are an advanced feature. An **understanding of how Unity works is required** to utilize them.
 ---A Component is a collection of functions and variables that allow you to control object behavior.
 ---
----Components are a [Unity concept](https://docs.unity3d.com/Manual/Components.html), they're the building blocks that Tabletop Simulator objects are composed of.<br><br>
+---Components are a [Unity concept](https://docs.unity3d.com/Manual/Components.html), they're the building blocks that
+---Tabletop Simulator objects are composed of.
 ---## Components
----GameObjects are themselves made up of [Components](https://api.tabletopsimulator.com/components/component/). A typical GameObject would contain a Collider, Transform, Mesh etc. These Components describe the GameObject's behavior and visual representation.
+---GameObjects are themselves made up of [Components](https://api.tabletopsimulator.com/components/component/). A
+---typical GameObject would contain a Collider, Transform, Mesh etc. These Components describe the GameObject's
+---behavior and visual representation.
 ---
---->### Tip
---->In addition to built\-in Objects, the Component APIs provide access the GameObjects and Components that exist in AssetBundles. This means that when creating an AssetBundle, you may attach all manner of components (lights, sounds etc.) and you'll be able to control them via these APIs.<br><br>
+--->### 🔥 Tip
+--->In addition to built\-in Objects, the Component APIs provide access the GameObjects and Components that exist in
+---AssetBundles. This means that when creating an AssetBundle, you may attach all manner of components (lights, sounds
+---etc.) and you'll be able to control them via these APIs.
 ---
 ---## Vars
----Each Component has **Vars**. These are variables which you can modify to change how that Component affects the GameObject it composes.<br><br>
+---Each Component has **Vars**. These are variables which you can modify to change how that Component affects the
+---GameObject it composes.
 ---
 ---## Materials
----GameObjects with Renderer components typically also have attached [Materials](https://api.tabletopsimulator.com/components/material/), which govern the appearance of the object.
+---GameObjects with Renderer components typically also have attached
+---[Materials](https://api.tabletopsimulator.com/components/material/), which govern the appearance of the object.
 ---
 ---***
 ---
 ---[Open Documentation](https://api.tabletopsimulator.com/components/introduction/)
 
----Every object in Tabletop Simulator is a [GameObject](https://api.tabletopsimulator.com/components/gameobject/). When a game is created, GameObjects are loaded, initialized and some of these top-level GameObjects are then exposed via Tabletop Simulator's Lua scripting APIs as regular Objects, each with their own scripting contexts.
+---Every object in Tabletop Simulator is a [GameObject](https://api.tabletopsimulator.com/components/gameobject/). When
+---a game is created, GameObjects are loaded, initialized and some of these top-level GameObjects are then exposed via
+---Tabletop Simulator's Lua scripting APIs as regular Objects, each with their own scripting contexts.
 ---
----Some Lua-exposed Objects are made-up of hierarchy of children GameObjects. The Component APIs allow to access and interact with these children GameObjects (which you'd otherwise be unable to control).
+---Some Lua-exposed Objects are made-up of hierarchy of children GameObjects. The Component APIs allow to access and
+---interact with these children GameObjects (which you'd otherwise be unable to control).
 ---
 ---***
 ---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/)
----@class GameObject
+---@class GameObject: userdata
 ---@field name string The name of the GameObject
----@field getChild fun(name: string):GameObject Returns a child GameObject matching the specified `name`.
----@field getChildren fun(name:string):GameObject[] Returns the list of children GameObjects.
----@field getComponent fun(name: string): Component Returns a Component matching the specified `name` from the GameObject's list of Components.
----@field getComponentInChildren fun(name: string): Component Returns a Component matching the specified `name`. Found by searching the Components of the GameObject and its [children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first).
----@field getComponents fun(name?: string):Component[] Returns the GameObject's list of Components. `name` is optional, when specified only Components with specified `name` will be included.
----@field getComponentsInChildren fun(name?: string): Component[] Returns a list of Components found by searching the GameObject and its [children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first). `name` is optional, when specified only Components with specified `name` will be included.
----@field getMaterials fun(): Material[] Returns the GameObject's list of Materials.
----@field getMaterialsInChildren fun(): Material[] Returns a list of Materials found by searching the GameObject and its [children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first).
+local GameObject = {}
+
+---Return a child GameObject matching the specified `name`.
+---@param name string
+---@return GameObject
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getchild)
+function GameObject.getChild(name) end
+
+---Return the list of children GameObjects.
+---@param name string
+---@return GameObject[]
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getchildren)
+function GameObject.getChildren(name) end
+
+---Return a Component matching the specified `name` from the GameObject's list of Components.
+---@param name string
+---@return Component
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getcomponent)
+function GameObject.getComponent(name) end
+
+---Return a Component matching the specified `name`. Found by searching the Components of the GameObject and its
+---[children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first).
+---@param name string
+---@return Component
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getcomponentinchildren)
+function GameObject.getComponentInChildren(name) end
+
+---Return the GameObject's list of Components.
+---@param name? string When specified, only Components with this `name` will be included.
+---@return Component[]
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getcomponents)
+function GameObject.getComponents(name) end
+
+---Return a list of Components found by searching the GameObject and its
+---[children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first). 
+---@param name? string When specified, only Components with this `name` will be included.
+---@return Component[]
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getcomponentsinchildren)
+function GameObject.getComponentsInChildren(name) end
+
+---Return the GameObject's list of Materials.
+---@return Material[]
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getmaterials)
+function GameObject.getMaterials() end
+
+---Return a list of Materials found by searching the GameObject and its
+---[children](https://api.tabletopsimulator.com/components/gameobject/#getchildren) recursively (depth first).
+---@return Material[]
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/gameobject/#getmaterialsinchildren)
+function GameObject.getMaterialsInChildren() end
--- library/Object Components/Material.lua
+++ library/Object Components/Material.lua
@@ -3,9 +3,37 @@
 ---# Material
 ---
 ---The Material of a Renderer [component](https://api.tabletopsimulator.com/components/component/) is the primary method of controlling that object's appearance.
----@class Material
+---@class Material: userdata
 ---@field game_object GameObject The GameObject the Material is attached to.
 ---@field shader string The name of the shader used by the Material.
----@field get fun(name: string): any Obtains the value of a given Variable on a Material.
----@field getVars fun():table<string, string> Returns a table mapping Var names (`string`) to their type, which is also represented as a `string`.
----@field set fun(name: string, value: any): bool Sets toe Var of the specified `name` to the provided `value`.
+local Material = {}
+
+---Obtain the value of a given Variable on a Material
+---@param name string
+---@return any
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/material/#get)
+---
+function Material.get(name) end
+
+---Returns a table mapping Var names to their type
+---@return table<string, string>
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/material/#getvars)
+---
+function Material.getVars() end
+
+---Sets the Var of the specified `name` to the provided `value`
+---@param name string
+---@param value any
+---@return bool
+---
+---***
+---
+---[Open Documentation](https://api.tabletopsimulator.com/components/material/#set)
+---
+function Material.set(name, value) end

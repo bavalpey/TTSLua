8406ecffd1c570bec6a628f7ea502e862c5f0f52
Pezeee <pezeee001@gmail.com>
2025-08-17 20:26:07 +0400

    Added undocumented note to new VectorLine params

--- library/Misc/VectorLine.lua
+++ library/Misc/VectorLine.lua
@@ -10,8 +10,8 @@
 ---@field color Color The color of the line.
 ---@field thickness float How thick the line is (in Unity units). *Optional, defaults to default line size (0.1).*
 ---@field rotation Vector The rotation of the line. *Optional, defaults to {x=0, y=0, z=0}*
----@field loop bool If true, the end point is connected to the start point. *Optional, defaults to false*
----@field square bool If true, line ends aren't rounded. *Optional, default to false*
+---@field loop bool (undocumented by official API) If true, the end point is connected to the start point. *Optional, defaults to false*
+---@field square bool (undocumented by official API) If true, line ends aren't rounded. *Optional, default to false*
 
 ---Parameters for the `Global.setVectorLines` and `Object.setVectorLines` functions.
 ---@see Object.setVectorLines
@@ -20,5 +20,5 @@
 ---@field color? ColorLike The color the lines should be. *Optional, defaults to {r=1, g=1, b=1, a=1} (white).*
 ---@field thickness? float How thick to make the line (in Unity units). *Optional, defaults to default line size (0.1).*
 ---@field rotation? VectorLike The rotation of the lines. *Optional, defaults to {x=0, y=0, z=0}*
----@field loop? bool If true, the end point is connected to the start point. *Optional, defaults to false*
----@field square? bool If true, line ends aren't rounded. *Optional, default to false*
+---@field loop? bool (undocumented by official API) If true, the end point is connected to the start point. *Optional, defaults to false*
+---@field square? bool (undocumented by official API) If true, line ends aren't rounded. *Optional, default to false*
